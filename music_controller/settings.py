2f94927d734c156097889e2699c14d2c999da8e8
Seemo <ousama_allahham@hotmail.com>
2024-02-11 13:04:24 -0500

    Tell Django to look for static files

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'static'),
+]
 MEDIA_URL = 'media/'
 
 # Default primary key field type
